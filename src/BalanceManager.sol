8bcdc1b565b1778057c51c624a067ad7ecfa1b33
Jon Bray <jon@heyjonbray.com>
2024-07-10 21:32:47 -0400

    add all balances getter; update comments

--- src/BalanceManager.sol
+++ src/BalanceManager.sol
@@ -11,20 +11,21 @@ contract BalanceManager is Ownable, ReentrancyGuard {
     mapping(address => uint256) public totalBalances;
     mapping(address => address[]) public walletTokens;
     mapping(address => address[]) public tokenWallets;
+    address[] public allTokens;
 
-    // check if caller is admin
     modifier onlyAdmin() {
         require(admins[msg.sender], "Caller is not an admin");
         _;
     }
 
-    // prevent contract itself from being the user
+    /**
+     * @dev modifier to ensure the contract itself cannot be a user
+     */
     modifier notContract(address user) {
         require(user != address(this), "Contract cannot be the user");
         _;
     }
 
-    // events to log balance changes
     event AdminAdded(address indexed admin);
     event AdminRemoved(address indexed admin);
     event BalanceSet(address indexed user, address indexed token, uint256 balance);
@@ -36,19 +37,21 @@ contract BalanceManager is Ownable, ReentrancyGuard {
 
     constructor(address initialOwner) Ownable(initialOwner) {}
 
-    // function to allow owner to add admin role
     function addAdmin(address admin) external onlyOwner {
         admins[admin] = true;
         emit AdminAdded(admin);
     }
 
-    // function to allow owner to remove admin role
     function removeAdmin(address admin) external onlyOwner {
         admins[admin] = false;
         emit AdminRemoved(admin);
     }
 
-    // function to allow admin to set balance
+    /**
+     * @dev Sets the balance of `user` for `token`
+     * @dev only admin can set balance
+     * @param amount The amount to set
+     */
     function setBalance(address user, address token, uint256 amount) external onlyAdmin notContract(user) {
         require(user != address(0), "Invalid user address");
         require(token != address(0), "Invalid token address");
@@ -57,6 +60,9 @@ contract BalanceManager is Ownable, ReentrancyGuard {
         if (currentBalance == 0 && amount > 0) {
             walletTokens[user].push(token);
             tokenWallets[token].push(user);
+            if (totalBalances[token] == 0) {
+                allTokens.push(token);
+            }
         }
 
         if (amount > currentBalance) {
@@ -69,7 +75,6 @@ contract BalanceManager is Ownable, ReentrancyGuard {
         emit BalanceSet(user, token, amount);
     }
 
-    // function to allow admin to increase balance
     function increaseBalance(address user, address token, uint256 amount) external onlyAdmin notContract(user) {
         require(user != address(0), "Invalid user address");
         require(token != address(0), "Invalid token address");
@@ -77,6 +82,9 @@ contract BalanceManager is Ownable, ReentrancyGuard {
         if (balances[user][token] == 0 && amount > 0) {
             walletTokens[user].push(token);
             tokenWallets[token].push(user);
+            if (totalBalances[token] == 0) {
+                allTokens.push(token);
+            }
         }
 
         balances[user][token] += amount;
@@ -84,7 +92,6 @@ contract BalanceManager is Ownable, ReentrancyGuard {
         emit BalanceIncreased(user, token, amount);
     }
 
-    // function to allow admin to reduce balance
     function reduceBalance(address user, address token, uint256 amount) external onlyAdmin notContract(user) {
         require(user != address(0), "Invalid user address");
         require(token != address(0), "Invalid token address");
@@ -95,15 +102,24 @@ contract BalanceManager is Ownable, ReentrancyGuard {
         emit BalanceReduced(user, token, amount);
     }
 
-    // function to allow any wallet to fund the contract
+    /**
+     * @dev allow any user to fund the contract
+     * @dev balance must still be set by admin
+     */
     function fund(address token, uint256 amount) external {
         require(token != address(0), "Invalid token address");
         require(amount > 0, "Amount must be greater than zero");
+        if (totalBalances[token] == 0) {
+            allTokens.push(token);
+        }
         IERC20(token).transferFrom(msg.sender, address(this), amount);
         emit Funded(token, amount);
     }
 
-    // function to allow user to claim balance
+    /**
+     * @dev allows a user to claim their balance of a certain token
+     * @param token token to claim balance of
+     */
     function claim(address token) public notContract(msg.sender) nonReentrant {
         require(token != address(0), "Invalid token address");
         uint256 balance = balances[msg.sender][token];
@@ -115,7 +131,9 @@ contract BalanceManager is Ownable, ReentrancyGuard {
         IERC20(token).transfer(msg.sender, balance);
     }
 
-    // function to allow user to claim all balances
+    /**
+     * @dev allows a user to claim their balance of all tokens
+     */
     function claimAll() external notContract(msg.sender) nonReentrant {
         uint256 length = walletTokens[msg.sender].length;
         require(length > 0, "No balances available to claim");
@@ -132,7 +150,13 @@ contract BalanceManager is Ownable, ReentrancyGuard {
         }
     }
 
-    // function to allow owner to withdraw stuck tokens
+    /**
+     * @dev allows admin to withdraw excess tokens
+     * @dev only tokens not assigned to a balance can be withdrawn
+     * @param token address of target token
+     * @param amount amount of token to withdraw
+     * @param to address of recipient account
+     */
     function withdrawExcessTokens(address token, uint256 amount, address to) external onlyOwner {
         require(token != address(0), "Invalid token address");
         require(to != address(0), "Invalid recipient address");
@@ -144,7 +168,7 @@ contract BalanceManager is Ownable, ReentrancyGuard {
         emit TokensWithdrawn(token, amount, to);
     }
 
-    // get balance for a specific (wallet, token) combo
+    // get balance for a specific (wallet, token)
     function getBalance(address wallet, address token) external view returns (uint256) {
         return balances[wallet][token];
     }
@@ -171,5 +195,17 @@ contract BalanceManager is Ownable, ReentrancyGuard {
             balanceValues[i] = balances[wallets[i]][token];
         }
         return (wallets, balanceValues);
+    }
+
+    // get all balances of all tokens
+    function getAllTotalBalances() external view returns (address[] memory, uint256[] memory) {
+        uint256 length = allTokens.length;
+        address[] memory tokens = new address[](length);
+        uint256[] memory balanceValues = new uint256[](length);
+        for (uint256 i = 0; i < length; i++) {
+            tokens[i] = allTokens[i];
+            balanceValues[i] = totalBalances[tokens[i]];
+        }
+        return (tokens, balanceValues);
     }
 }
